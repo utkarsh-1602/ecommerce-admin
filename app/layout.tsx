15f6fcf45a27596b49800e49b695f9cc9594f760
utkarsh-1602 <usking619@gmail.com>
2023-11-07 18:58:56 +0530

    added : toast notification provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { ModalProvider } from '@/providers/modal-provider'
+import { ToasterProvider } from '@/providers/toast-provider'
 
 import './globals.css'
-import prismadb from '@/lib/prismadb'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +26,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -11,6 +11,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
     name: z.string().min(1),
@@ -39,11 +40,11 @@ const StoreModal = () => {
 
             const response = await axios.post('/api/stores', values);
 
-            console.log(response.data);
+            toast.success("Store created")
 
 
         } catch (error) {
-            console.log(error);
+            toast.error("Something went wrong")
         }
         finally {
             setLoading(false);
