586a0874f9c6c59b0e5d5377dfc5a432d0d798c3
utkarsh-1602 <usking619@gmail.com>
2023-11-07 13:22:06 +0530

    refactor code : hydration error solved

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,24 @@
 "use client"
 
 import { Modal } from "@/components/ui/modal";
+import { useEffect, useState } from 'react';
 
 const SetupPage = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        setIsModalOpen(true);
+    }, []);
+
     return (
         <div className="flex items-center justify-center h-full">
-            <Modal title="Test" description="Test Desc" isOpen onClose={() => { }}>
-                Children
-            </Modal>
+            {isModalOpen && (
+                <Modal title="Test" description="Test Desc" isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                    Children
+                </Modal>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default SetupPage;
