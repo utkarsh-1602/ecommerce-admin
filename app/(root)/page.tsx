c0ba962d8dbff92fcbe4b6bbfad705e32f9832a8
utkarsh-1602 <usking619@gmail.com>
2023-11-07 10:58:42 +0530

    functionality: modal component, dialog shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 export default function SetupPage() {
     return (
         <div className="flex items-center justify-center h-full">
-            <UserButton afterSignOutUrl="/" />
-        </div>
+            <Modal onClose={() => { }} isOpen={true} title="Test" description="Test Desc">
+                Children
+            </Modal>
+            {/* <UserButton /> */}
+        </div >
     )
 }
