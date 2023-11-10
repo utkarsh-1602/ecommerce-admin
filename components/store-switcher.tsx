297fa51bf87ab3a86946f9a4882789cefda8a56e
utkarsh-1602 <usking619@gmail.com>
2023-11-10 23:44:22 +0530

    modified: store-switcher component

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import { Store } from "@prisma/client"
-
-import { PopoverTrigger } from "@/components/ui/popover"
-import { UseStoreModal } from "@/hooks/use-store-modal";
 import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+
+import { Popover, PopoverTrigger } from "@/components/ui/popover"
+import { UseStoreModal } from "@/hooks/use-store-modal";
+import { useState } from "react";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -18,10 +20,32 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
 
     const storeModal = UseStoreModal();
     const params = useParams();
+    const router = useRouter();
+
+    // iterate over all items
+    const formattedItems = items.map((item) => ({
+        label: item.name,
+        value: item.id
+    }))
+
+    // which is the currently active store from all stores 
+    const currentStore = formattedItems.find((item) => item.value === params.storeId)
+
+
+    const [open, setOpen] = useState(false);
+
+    // function that will trigger when what happens when we click on different store 
+    const onStoreSelect = (store: { value: string, label: string }) => {
+        setOpen(false);
+        router.push(`/${store.value}`);
+    }
 
     return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
 
-        <div>StoreSwitcher</div>
+            </PopoverTrigger>
+        </Popover>
     )
 }
 
