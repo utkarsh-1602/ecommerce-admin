6ee92eb38142992a5839b82f1f0293baa696ce3e
utkarsh-1602 <usking619@gmail.com>
2023-11-13 13:46:30 +0530

    added: currentStore button using popover ui

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -10,22 +10,34 @@ export function MainNav({
 }: React.HTMLAttributes<HTMLElement>) {
 
     const pathName = usePathname();
+    console.log("pathName ==> ", pathName)
+
+
     const params = useParams();
+    console.log("Params ===> " + JSON.stringify(params));
+
+
     const routes = [{
         href: `/${params.storeId}/settings`,
         label: "Settings",
         active: pathName === `/${params.storeId}/settings`
     }];
 
+    console.log("routes ====>" + routes);
+
     return (
         <nav
-            className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+            className={cn("flex space-x-4 lg:space-x-6", className)}
         >
             {routes.map((route) => (
                 <Link
                     key={route.href}
                     href={route.href}
-                    className={cn("text-sm font-medium transition-colors hover:text-primary", route.active ? "text-black dark:text-white" : "text-muted-foreground")}
+                    className={cn(
+                        "text-sm font-medium transition-colors hover:text-primary",
+                        route.active ? "text-black dark:text-white" : "text-muted-foreground",
+                        "ml-6"
+                    )}
                 >
 
                     {route.label}
