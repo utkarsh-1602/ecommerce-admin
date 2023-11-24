a224f25e264188ba631ad1c7d18b53e7301a17ef
utkarsh-1602 <usking619@gmail.com>
2023-11-25 00:03:44 +0530

    added: billboard in categories api route

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -19,6 +19,9 @@ export async function GET(
             where: {
                 id: params.categoryId,
             },
+            include: {
+                billboard: true
+            }
         })
 
         return NextResponse.json(categories)
