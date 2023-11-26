19c6506bde6dca9bcc85d0d748681d0c8c69d89b
utkarsh-1602 <usking619@gmail.com>
2023-11-26 13:55:52 +0530

    comments on zustand code

--- hooks/use-store-modal.tsx
+++ hooks/use-store-modal.tsx
@@ -1,13 +1,15 @@
 import { create } from "zustand";
 
+// Defining an interface for the state of the modal store
 interface useStoreModalStore {
-    isOpen: boolean;
-    onOpen: () => void;
-    onClose: () => void;
+    isOpen: boolean; // Indicates whether the modal is open or closed
+    onOpen: () => void;   // Function to open the modal
+    onClose: () => void; // Function to close the modal
 }
 
+// Creating a Zustand store for managing the modal state
 export const UseStoreModal = create<useStoreModalStore>((set) => ({
-    isOpen: false,
-    onOpen: () => set({ isOpen: true }),
-    onClose: () => set({ isOpen: false })
+    isOpen: false, // Initial state: modal is closed
+    onOpen: () => set({ isOpen: true }), // Function to open the modal by setting `isOpen` to `true`
+    onClose: () => set({ isOpen: false }) // Function to close the modal by setting `isOpen` to `false`
 }))
