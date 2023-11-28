e842dfee028b105b826f6c0de559f474b6e177a4
utkarsh-1602 <usking619@gmail.com>
2023-11-29 00:17:29 +0530

    completed : dashboard overview page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { getGraphRevenue } from "@/actions/get-graph-revenue"
 import { getSalesCount } from "@/actions/get-sales-count"
 import { getStockCount } from "@/actions/get-stock-count"
 import { getTotalRevenue } from "@/actions/get-total-revenue"
@@ -18,9 +19,9 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
 }) => {
 
     const totalRevenue = await getTotalRevenue(params.storeId)
+    const graphRevenue = await getGraphRevenue(params.storeId)
     const salesCount = await getSalesCount(params.storeId)
     const stockCount = await getStockCount(params.storeId)
-
 
     return (
         <div className="flex-col">
@@ -63,7 +64,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                         <CardTitle>Overview</CardTitle>
                     </CardHeader>
                     <CardContent className="pl-2">
-                        <Overview data={[]} />
+                        <Overview data={graphRevenue} />
                     </CardContent>
                 </Card>
             </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ModalProvider } from '@/providers/modal-provider'
 import { ToasterProvider } from '@/providers/toast-provider'
 
 import './globals.css'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,9 +27,16 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
