5163308480741346e06c23f96086d1b067ff2a6f
utkarsh-1602 <usking619@gmail.com>
2023-11-29 00:03:58 +0530

    added : get sales count, stock count, total revenue

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,7 @@
+import { getSalesCount } from "@/actions/get-sales-count"
+import { getStockCount } from "@/actions/get-stock-count"
+import { getTotalRevenue } from "@/actions/get-total-revenue"
+import Overview from "@/components/overview"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import Heading from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
@@ -13,9 +17,9 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
     params
 }) => {
 
-    const totalRevenue = () => { };
-    const salesCount = () => { };
-    const stockCount = () => { };
+    const totalRevenue = await getTotalRevenue(params.storeId)
+    const salesCount = await getSalesCount(params.storeId)
+    const stockCount = await getStockCount(params.storeId)
 
 
     return (
@@ -32,7 +36,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                             <DollarSign className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
                         <CardContent>
-                            <div className="text-2xl font-bold">{formatter.format(200)}</div>
+                            <div className="text-2xl font-bold">{formatter.format(totalRevenue)}</div>
                         </CardContent>
                     </Card>
                     <Card>
@@ -41,7 +45,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                             <CreditCard className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
                         <CardContent>
-                            <div className="text-2xl font-bold">+25</div>
+                            <div className="text-2xl font-bold">+{salesCount}</div>
                         </CardContent>
                     </Card>
                     <Card>
@@ -50,7 +54,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                             <Package className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
                         <CardContent>
-                            <div className="text-2xl font-bold">12</div>
+                            <div className="text-2xl font-bold">{stockCount}</div>
                         </CardContent>
                     </Card>
                 </div>
@@ -59,7 +63,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                         <CardTitle>Overview</CardTitle>
                     </CardHeader>
                     <CardContent className="pl-2">
-                        {/* <Overview data={graphRevenue} /> */}
+                        <Overview data={[]} />
                     </CardContent>
                 </Card>
             </div>
